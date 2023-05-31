32a314f1b5d409b950ff9931f7560e806da40d32
davidperlaza14 <davidperlaza14@gmail.com>
2023-05-31 15:33:44 -0500

    IA JARVIS IROMAN

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # J.A.R.V.I.S_IRO_MAN
+
+
+## Si, leíste bien. Podemos crear nuestro propio J.A.R.V.I.S., usando Python. ¡Vamos a ello!
---
+++ funciones/os_ops.py
@@ -0,0 +1,8 @@
+import os
+import subprocess as sp
+
+paths = {
+    'mozilla': "/home/davidperlaza14/.mozilla",
+    'discord': "C:\\Users\\ashut\\AppData\\Local\\Discord\\app-1.0.9003\\Discord.exe",
+    'calculator': "C:\\Windows\\System32\\calc.exe"
+}
